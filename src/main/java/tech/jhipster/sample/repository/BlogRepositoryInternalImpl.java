1b76c6dcc271446ddcabcf1bca5fc317311762f4
Enrico Costanzi <enrico.costanzi@gmail.com>
2021-01-24 12:51:01 +0100

    fix blogrepository

--- src/main/java/tech/jhipster/sample/repository/BlogRepositoryInternalImpl.java
+++ src/main/java/tech/jhipster/sample/repository/BlogRepositoryInternalImpl.java
@@ -1,26 +1,24 @@
 package tech.jhipster.sample.repository;
 
+import static org.springframework.data.relational.core.query.Criteria.where;
+
 import io.r2dbc.spi.Row;
 import io.r2dbc.spi.RowMetadata;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.function.BiFunction;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.r2dbc.core.DatabaseClient;
-import org.springframework.data.r2dbc.core.DatabaseClient.GenericInsertSpec;
-import org.springframework.data.r2dbc.core.ReactiveDataAccessStrategy;
-import org.springframework.data.r2dbc.core.RowsFetchSpec;
-import org.springframework.data.r2dbc.mapping.OutboundRow;
-import org.springframework.data.r2dbc.mapping.SettableValue;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
+import org.springframework.data.relational.core.query.Update;
 import org.springframework.data.relational.core.sql.Column;
 import org.springframework.data.relational.core.sql.Expression;
 import org.springframework.data.relational.core.sql.Select;
 import org.springframework.data.relational.core.sql.SelectBuilder.SelectFromAndJoinCondition;
 import org.springframework.data.relational.core.sql.Table;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.core.RowsFetchSpec;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tech.jhipster.sample.domain.Blog;
@@ -35,7 +33,7 @@ import tech.jhipster.sample.service.EntityManager;
 class BlogRepositoryInternalImpl implements BlogRepositoryInternal {
 
     private final DatabaseClient db;
-    private final ReactiveDataAccessStrategy dataAccessStrategy;
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
     private final EntityManager entityManager;
 
     private final UserRowMapper userMapper;
@@ -45,14 +43,13 @@ class BlogRepositoryInternalImpl implements BlogRepositoryInternal {
     private static final Table userTable = Table.aliased("jhi_user", "e_user");
 
     public BlogRepositoryInternalImpl(
-        DatabaseClient db,
-        ReactiveDataAccessStrategy dataAccessStrategy,
+        R2dbcEntityTemplate template,
         EntityManager entityManager,
         UserRowMapper userMapper,
         BlogRowMapper blogMapper
     ) {
-        this.db = db;
-        this.dataAccessStrategy = dataAccessStrategy;
+        this.db = template.getDatabaseClient();
+        this.r2dbcEntityTemplate = template;
         this.entityManager = entityManager;
         this.userMapper = userMapper;
         this.blogMapper = blogMapper;
@@ -95,7 +92,7 @@ class BlogRepositoryInternalImpl implements BlogRepositoryInternal {
                         .toString()
             )
             .orElse(select); // TODO remove once https://github.com/spring-projects/spring-data-jdbc/issues/907 will be fixed
-        return db.execute(selectWhere).map(this::process);
+        return db.sql(selectWhere).map(this::process);
     }
 
     @Override
@@ -105,7 +102,7 @@ class BlogRepositoryInternalImpl implements BlogRepositoryInternal {
 
     @Override
     public Mono<Blog> findById(Long id) {
-        return createQuery(null, Criteria.where("id").is(id)).one();
+        return createQuery(null, where("id").is(id)).one();
     }
 
     private Blog process(Row row, RowMetadata metadata) {
@@ -138,7 +135,11 @@ class BlogRepositoryInternalImpl implements BlogRepositoryInternal {
 
     @Override
     public Mono<Integer> update(Blog entity) {
-        return db.update().table(Blog.class).using(entity).fetch().rowsUpdated();
+        return r2dbcEntityTemplate.update(
+            Query.query(where("id").is(entity.getId())),
+            Update.update("name", entity.getName()).set("user_id", entity.getUserId()),
+            Blog.class
+        );
     }
 }
 
